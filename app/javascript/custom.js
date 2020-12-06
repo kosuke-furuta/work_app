3a75a2d17bdf4ea513c9c288708ba0a26bfa00d9
kosuke-furuta <furu.21.22@gmail.com>
2020-12-07 07:01:48 +0900

    jQuery javascript CSS webpacker導入

--- app/javascript/custom.js
+++
@@ -1,7 +0,0 @@
-$(".tab_label").on("click",function(){
-	var $th = $(this).index();
-	$(".tab_label").removeClass("active");
-	$(".tab_panel").removeClass("active");
-	$(this).addClass("active");
-	$(".tab_panel").eq($th).addClass("active");
-});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,14 +10,15 @@ require("channels")
 
 // 追加
 require("jquery")
-import "bootstrap"
-require('serach')
+import 'bootstrap'
+import '../stylesheets/application'
+import '../js/application'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
 //= require rails-ujs
